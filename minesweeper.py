16883748cafcf0bb0758d58456778a5ad93f78d1
Simon <simon_sch92@web.de>
2021-01-04 15:30:48 +0100

    updated exe creater, deleted obsolete files

--- minesweeper.py
+++ minesweeper.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 
 
-import os
 import sys
 import gui
 from classes import GameSetting, Game
@@ -11,29 +10,13 @@ from classes import GameSetting, Game
 # debug mode evaluation, enabling some more printouts, enabling console
 DEBUG = (True if '--debug' in sys.argv else False)
 
-# hide console as non debug option
-if not DEBUG:
-    try:
-        import win32gui
-        import win32con
-        the_program_to_hide = win32gui.GetForegroundWindow()
-        win32gui.ShowWindow(the_program_to_hide, win32con.SW_HIDE)
-    except ModuleNotFoundError:
-        os.system("pip install pywin32")
-        import win32gui
-        import win32con
-        the_program_to_hide = win32gui.GetForegroundWindow()
-        win32gui.ShowWindow(the_program_to_hide, win32con.SW_HIDE)
-    except Exception as e:
-        print("Could not hide console:", e)
-
 
 # todo am ende aufräumen und unbenutzte variablen aufräumen/löschen
 # todo alles was nicht notwendig ist nicht als klassenvariabe machen
 # todo ini/jsom file with highscores and last gamesetting selected
 
 
-AVAILABLE_MODES = {  # todo different game settings: Beginner, Intermediate, ...
+AVAILABLE_MODES = {
     'Beginner': GameSetting(rows=9, cols=9, bombs=10),
     'Intermediate': GameSetting(rows=16, cols=16, bombs=40),
     'Expert': GameSetting(rows=16, cols=30, bombs=99),
@@ -77,7 +60,6 @@ class Minesweeper:
 if __name__ == '__main__':
 
     if '--debug' in sys.argv:
-        print("######################################")
         print("Starting game in DEBUG Mode:")
 
     mw = Minesweeper()
---
+++ mk_exe.bat
@@ -0,0 +1,28 @@
+@echo off & setlocal
+
+pyinstaller --name Minesweeper^
+   --paths "%cd%"^
+   --onefile ^
+   --noconsole ^
+   --clean ^
+   --add-data "%cd%/ui";"." ^
+   --add-data "%cd%/ui/icon.ico";"./ui" ^
+   --add-data "%cd%/ui/counter";"./ui" ^
+   --add-data "%cd%/ui/smiley";"./ui" ^
+   --add-data "%cd%/ui/fields";"./ui" ^
+   --add-data "%cd%/ui/fields/front";"./ui/fields" ^
+   --add-data "%cd%/ui/fields/back";"./ui/fields" ^
+   --add-data "%cd%/ui/counter/*.png";"./ui/counter" ^
+   --add-data "%cd%/ui/smiley/*.png";"./ui/smiley" ^
+   --add-data "%cd%/ui/fields/front/*.png";"./ui/fields/front" ^
+   --add-data "%cd%/ui/fields/back/*.png";"./ui/fields/back" ^
+   --icon "%cd%/ui/icon.ico" ^
+   ./minesweeper.py
+   
+   
+   
+   
+RMDIR /S /Q %cd%\build
+RMDIR /S /Q %cd%\__pycache__
+RMDIR /S /Q %cd%\.idea
+del %cd%\Minesweeper.spec
